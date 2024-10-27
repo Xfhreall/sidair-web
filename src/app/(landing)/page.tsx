d87d844012a9908a35333c02a77b8bbf6909faaa
Risqi Fahreal <106820529+Xfhreall@users.noreply.github.com>
2024-10-27 20:30:54 +0700

    deploys

--- src/app/(landing)/page.tsx
+++ src/app/(landing)/page.tsx
@@ -2,16 +2,12 @@ import React from "react";
 import Hero from "@/components/home/section/hero";
 import About from "@/components/home/section/about";
 import Watesqy from "@/components/home/section/watesqy";
-import Summary from "@/components/home/section/summary";
-import Filtrasi from "@/components/home/section/filtrasi";
 export default function Page() {
   return (
     <>
       <Hero />
       <About />
       <Watesqy />
-      <Summary />
-      <Filtrasi />
     </>
   );
 }
